f1b2e0c9570d3f90700dc51c689d6b011bc04ec4
ComandPromt <vayase_a_ferrol@hotmail.com>
2020-08-12 13:31:19 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function limpiar_estrellas(){
-
+document.getElementById("rate-string").textContent='';
   for(var i =1;i<=5;i++){
     document.getElementsByClassName('r'+i)[0].classList.remove("fullstar");
   }
@@ -7,7 +7,13 @@ function limpiar_estrellas(){
 }
 
 (function() {
+  'use strict';
+  var host = './';
 
+	var scripts = document.getElementsByTagName('script');
+	var scriptName = scripts[scripts.length-1];
+
+  var rWrap = document.getElementById(scriptName.getAttribute('data-id'));
   var ratings = {
     1: 'Nicht gut',
     2: 'Naja',
@@ -19,12 +25,21 @@ function limpiar_estrellas(){
   var stars = document.getElementsByClassName('star');
 
   function fillStars(mynode) {
+	  
     // Is it a node or already a rating?
+    
     var num;
+    
     if(Number.isInteger(mynode)) {
       num = mynode;
-    } else {
-      num = mynode.className.match(/\d+/)[0];
+    }
+    
+    else {
+		
+		if(mynode!=undefined){
+			num = mynode.className.match(/\d+/)[0];
+		}
+
     }
 
     emptyStars()
@@ -37,7 +52,26 @@ function limpiar_estrellas(){
 
   }
 
+  function sendRating(mynode) {
+    var num = mynode.className.match(/\d+/)[0];
+
+    fetch(host+'/rater.php?&id='+num)
+    .catch(function(error) {
+      console.log('could not fetch: ' + error);
+    })
+    .then(function(str) {
+      return str.text();
+    })
+    .then(function(result) {
+      document.getElementById("rate-string").textContent = result;
+    })
+    .catch(function(err) {
+      console.log(err);
+    })
+  }
+
   function getRating(id) {
+
     fetch(host+'/rater.php?id='+id)
     .then(function(result) {
       result.json()
@@ -57,7 +91,7 @@ function limpiar_estrellas(){
           else {
             bewstring = "Bewertung";
           }
-          document.getElementById("rate-string").textContent = data.votes + " " + bewstring;
+          document.getElementById("rate-string").textContent = bewstring;
         });
       });
     });
@@ -94,14 +128,16 @@ function pintar(numero){
 
 }
 
-
-
   pintar(0); // cambia el 0 por el número de estrellas por defecto
 
   for(var i = 0; i < stars.length; i++) {
 
    stars[i].addEventListener('mouseover', fillStars.bind(this, stars[i]));
- 
+   
+   stars[i].addEventListener('click', sendRating.bind(this, stars[i]));
+   
   }
-
+  
+  getRating(rWrap.id);
+  
 })();
--- rater.php
+++ rater.php
@@ -9,34 +9,36 @@
 	switch($id){
 		
 		case 1:
-		
+		$resultado='1';
 		break;
 		
 		case 2:
-		
+		$resultado='2';
 		break;
 		
 		case 3:
-		
+		$resultado='3';
 		break;
 		
 		case 4:
-		
+		$resultado='4';
 		break;
 		
 		case 5:
+		$resultado='5';
+		break;
 		
+		default:
+		 $resultado='';
 		break;
 		
 	}
-  
-      $resultado="aaa";
-      
-      print($resultado);
+     
+	echo  json_encode($resultado);
     
     
   }
 
-    deliver_rating($id);
+  deliver_rating($id);
 
 ?>
